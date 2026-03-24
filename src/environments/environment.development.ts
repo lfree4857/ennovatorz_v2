ffabacfbe76818eed18b82df261f26930110f00c
jiaulh <jiaulrbpo123@gmail.com>
2026-03-24 22:24:59 +0530

    blog crud is done!

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001',
-  uploadsUrl: ''
+  // apiUrl: 'http://localhost:3001',
+  // uploadsUrl: ''
+  apiUrl: 'https://devapi.ennovatorz.com',
+  uploadsUrl: 'https://devapi.ennovatorz.com'
 };
