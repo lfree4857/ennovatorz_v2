b91adee39346243b52d4a56d5acfc992fdbab3ce
jiaulh <jiaulrbpo123@gmail.com>
2026-03-25 20:48:25 +0530

    phone no and email added!

--- src/app/shared/constants/contact-info.constant.ts
+++ src/app/shared/constants/contact-info.constant.ts
@@ -1,9 +1,9 @@
 export const CONTACT_INFO = {
-  email: 'hello@innovators.dev',
-  emailHref: 'mailto:hello@innovators.dev',
-  jobsEmail: 'jobs@innovators.dev',
-  jobsEmailHref: 'mailto:jobs@innovators.dev',
-  phone: '+1 (415) 000-0000',
-  phoneHref: 'tel:+14150000000',
-  address: '100 Innovation Way, San Francisco, CA 94105',
+  email: 'lfree4857@gmail.com',
+  emailHref: 'mailto:lfree4857@gmail.com',
+  jobsEmail: 'lfree4857@gmail.com',
+  jobsEmailHref: 'mailto:lfree4857@gmail.com',
+  phone: '+9160005 87566',
+  phoneHref: 'tel:+9160005 87566',
+  address: 'Guwahati,Kamrup Metro,Assam,781025',
 };
